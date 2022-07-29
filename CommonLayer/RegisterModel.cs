8a4944cd8d06c7ec0c80c7d53275230582a3e786
yash-agrwl <agarwalyash7012@gmail.com>
2022-07-29 08:03:15 +0530

    implemented 4 layer architecture & added register, login, response models

---
+++ CommonLayer/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommonLayer
+{
+    public class RegisterModel
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public long MobileNumber { get; set; }
+    }
+}
---
+++ RepositoryLayer/Repository/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using CommonLayer;
+using Microsoft.Extensions.Configuration;
+
+namespace RepositoryLayer.Repository
+{
+    public class UserRepository
+    {
+        public IConfiguration Configuration { get; }
+
+        public UserRepository(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public ResponseModel<RegisterModel> Register(RegisterModel userData)
+        {
+        }
+    }
+}
